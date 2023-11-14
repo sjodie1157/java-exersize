e21b884514902c0483eb4d39cf70a59c742cd53f
sjodie1157 <sjodie1157@gmail.com>
2023-11-14 16:40:20 +0200

    fixed math floor

--- script/code.js
+++ script/code.js
@@ -1,6 +1,7 @@
 
 let numbers = [12, 18, 11, 5, 8, 2];
 console.log(numbers);
+midNumber(numbers)
 
 function midNumber(numbers) {
     let length = numbers.length;
@@ -15,7 +16,6 @@ function midNumber(numbers) {
     }
 }
 
-midNumber(numbers)
 
 
   
